895a85c63fbba0ca7910a22f20205c2e3424ea60
Damon Vinciguerra <davinciguerra@Dcpmidstream.com>
2023-03-23 15:59:49 -0600

    I can return html files!

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const port = 8080;
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-const port = parseInt(process.env.PORT) || 8080;
+
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
