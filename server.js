a4310f864cc584586b24e343b1e7b24d0ff2a0d1
Damon Vinciguerra <davinciguerra@Dcpmidstream.com>
2023-03-24 16:30:47 -0600

    jsdelivr option

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import express from 'express'
 import {getFullFilePath} from './utility.js';
-
+import favicon from 'serve-favicon'
 
 const app = express();
-
 
 app.get('/', (req, res) => {
   res.sendFile(getFullFilePath('calendar.html'));
@@ -13,3 +12,5 @@ const port = 8080;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
+
+app.use(favicon(getFullFilePath('favicon.ico')))
